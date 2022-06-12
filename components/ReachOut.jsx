26e0d2d9d9cfdc6a2b7d98fd638a5d384a29ffb5
Nathan Galindo <nathan.zebedee@gmail.com>
2022-06-12 08:32:43 -0500

    edit to modal

--- components/ReachOut.jsx
+++ components/ReachOut.jsx
@@ -17,7 +17,7 @@ const ReachOut = ({ openContact, setOpenContact, cancelButtonRef }) => {
                     <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
                 </Transition.Child>
 
-                <div className="fixed z-10 sm:inset-0 inset-y-52 overflow-y-auto">
+                <div className="fixed z-10 inset-0 overflow-y-auto">
                     <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
                         <Transition.Child
                             as={Fragment}
