0b2f2efd65f574788304a8bdeb3128f7cb2b7d3e
Nathan Galindo <nathan.zebedee@gmail.com>
2022-06-12 08:29:12 -0500

    edit to maodal

--- components/Header/HeaderCategories.jsx
+++ components/Header/HeaderCategories.jsx
@@ -20,8 +20,8 @@ const HeaderCategories = ({ categoryType, setCategoryType }) => {
                     onClick={() => setCategoryType(category.type)}
                     className={
                         categoryType === category.type
-                            ? `flex cursor-pointer border-transparent text-[#302f3d] border-2 bg-gradient-to-r from-blue-100 to-purple-100 rounded-md h-20 justify-center items-center font-bold tracking-wider text-sm md:text-2xl lg:text-3xl`
-                            : `flex cursor-pointer hover:text-white hover:border-purple-300 text-gray-300 border-2 border-white rounded-md h-20 justify-center items-center font-bold tracking-wider text-sm md:text-2xl lg:text-3xl`
+                            ? `flex cursor-pointer border-transparent text-[#302f3d] border-2 border-purple-300 text-purple-300 rounded-md h-20 justify-center items-center font-bold tracking-wider text-sm md:text-2xl lg:text-3xl`
+                            : `flex cursor-pointer hover:border-purple-300 hover:text-purple-300 text-white border-2 border-white rounded-md h-20 justify-center items-center font-bold tracking-wider text-sm md:text-2xl lg:text-3xl`
                         }
                 >
                     <h4>{category.type}</h4>
